89ba48f87a71f1035d7c8c07d730298c4d812bbf
Cigi <j.cikanek@gmail.com>
2020-05-01 16:18:09 +0200

    Added lib

---
+++ dest/ScannerLoader.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by jcika on 11.02.2020.
+ **/
+import * as React from "react";
+declare const ScannerLoader: (Component: React.ComponentType<any>) => any;
+export default ScannerLoader;
